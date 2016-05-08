ee7b8077c14d3db731c0e47e2a78f747290f9d3d
Jean Paul BAMBANZA <jeanpok8@gmail.com>
2016-05-08 13:52:59 +0200

    Update Run.java

--- src/semanticsensors/Run.java
+++ src/semanticsensors/Run.java
@@ -1,14 +1,28 @@
-package ontology;
+package semanticsensors;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Run {
-	
-	//results=query.
-	
-	public static void main(String args[]){
-		
-	    Ontology ont=new Ontology();
-	    ont.scanner();
-		ont.query();
-				
-  }
+
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String args[]) throws IOException {
+		Reasoner reasoner = new Reasoner();
+		System.out.println("SELECT ONE USE CASE");
+		reasoner.mediumQuery("UseCase");
+		String input = scan.next();
+
+		switch (input) {
+		case "IReMHO":
+			Queriable technical = new Technical();
+			technical.queriable();
+
+		case "Infirmity":
+			Queriable queriable = new Ailments();
+			queriable.queriable();
+		    scan.close();
+		}
+
+	}
 }
